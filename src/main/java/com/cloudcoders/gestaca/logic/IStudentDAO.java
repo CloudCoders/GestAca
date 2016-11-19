5b0fbe54813f723fc92abd8e2d3536e7e4927818
Jose Llorens Ripolles <jose.llorens.ripolles@gmail.com>
2016-11-19 12:13:28 +0100

    Created StudentDAO interface

--- src/main/java/com/cloudcoders/gestaca/logic/IStudentDAO.java
+++ src/main/java/com/cloudcoders/gestaca/logic/IStudentDAO.java
@@ -1,4 +1,23 @@
 package com.cloudcoders.gestaca.logic;
 
-public class IStudentDAO {
+import com.cloudcoders.gestaca.model.Student;
+
+import java.util.List;
+
+public interface IStudentDAO {
+
+  Student getStudentBy(String dni);
+
+  List<Student> addAllStudents(List<Student> students);
+
+  Student addStudentBy(Student student);
+
+  Student removeStudentBy(Student student);
+
+  Student removeStudentBy(String dni);
+
+  List<Student> getAllStudents();
+
+  List<Student> removeAllStudents();
+
 }
