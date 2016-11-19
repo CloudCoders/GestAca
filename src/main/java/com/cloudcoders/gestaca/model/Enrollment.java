f75c0478a510b953871c1d247acfaa989d551f02
Antonio López Marín <tonilopezmr@gmail.com>
2016-11-19 02:35:21 +0100

    Renamed root project package

--- src/main/java/com/cloudcoders/gestaca/model/Enrollment.java
+++ src/main/java/com/cloudcoders/gestaca/model/Enrollment.java
@@ -1,4 +1,4 @@
-package model;
+package com.cloudcoders.gestaca.model;
 
 import java.util.ArrayList;
 import java.util.Date;
--- src/main/java/com/cloudcoders/gestaca/model/Person.java
+++ src/main/java/com/cloudcoders/gestaca/model/Person.java
@@ -1,4 +1,4 @@
-package model;
+package com.cloudcoders.gestaca.model;
 
 
 public abstract class Person {
