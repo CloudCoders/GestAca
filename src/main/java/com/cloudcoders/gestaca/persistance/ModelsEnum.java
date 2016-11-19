f55f0aabe4d025d419985d178b27fc11519092a0
Adriano Vega <adrianovega@hotmail.com>
2016-11-19 13:00:01 +0100

    Updated ModelsEnum to include the Class and the target file

--- src/main/java/com/cloudcoders/gestaca/persistance/ModelsEnum.java
+++ src/main/java/com/cloudcoders/gestaca/persistance/ModelsEnum.java
@@ -1,12 +1,26 @@
 package com.cloudcoders.gestaca.persistance;
 
+import com.cloudcoders.gestaca.model.*;
+
 public enum ModelsEnum {
-  ABSENCE,
-  COURSE,
-  ENROLLMENT,
-  OFFICE,
-  PERSON,
-  STUDENT,
-  TAUGHTCOURSE,
-  TEACER
+  ABSENCE("Absence.json", Absence.class),
+  COURSE("Course.json", Course.class),
+  ENROLLMENT("Enrollment.json", Enrollment.class),
+  OFFICE("Office.json", Office.class),
+  PERSON("Person.json", Person.class),
+  STUDENT("Student.json", Student.class),
+  TAUGHTCOURSE("TaughtCourse.json", TaughtCourse.class),
+  TEACHER("Teacher.json", Teacher.class);
+
+  private String archivo;
+  private Class clase;
+  ModelsEnum(String archivo, Class clase) {
+    this.archivo = archivo;
+    this.clase = clase;
+  }
+
+  public String getArchivo() {
+    return this.archivo;
+  }
+
 }
