49a15f32a2d917a95078acbf50d97a8a01e694c8
Antonio López Marín <tonilopezmr@gmail.com>
2016-11-19 17:39:46 +0100

    Added AddCourse use case in CreateCourseCommand

--- src/main/java/com/cloudcoders/gestaca/ui/controller/CreateCourseCommand.java
+++ src/main/java/com/cloudcoders/gestaca/ui/controller/CreateCourseCommand.java
@@ -1,5 +1,7 @@
 package com.cloudcoders.gestaca.ui.controller;
 
+import com.cloudcoders.gestaca.logic.course.AddCourse;
+import com.cloudcoders.gestaca.model.Course;
 import com.cloudcoders.gestaca.ui.View;
 
 public class CreateCourseCommand implements Command {
@@ -7,6 +9,12 @@ public class CreateCourseCommand implements Command {
   public static final String CREATE_CURSE = "crear curso";
 
   private View view;
+  private AddCourse addCourse;
+
+  public CreateCourseCommand(View view, AddCourse addCourse) {
+    this.view = view;
+    this.addCourse = addCourse;
+  }
 
   public CreateCourseCommand(View view) {
     this.view = view;
@@ -19,6 +27,7 @@ public class CreateCourseCommand implements Command {
 
   @Override
   public void execute() {
-
+    Course course = view.askCreateCourse();
+    addCourse.add(course);
   }
 }
