6a345e3aadbecce9da15dd024d4e38c01012446a
Antonio López Marín <tonilopezmr@gmail.com>
2016-11-20 17:31:13 +0100

    Renamed var to fileName

--- src/main/java/com/cloudcoders/gestaca/persistance/JsonParser.java
+++ src/main/java/com/cloudcoders/gestaca/persistance/JsonParser.java
@@ -9,18 +9,19 @@ import java.nio.file.Paths;
 
 public class JsonParser {
 
-  public JSONArray readFile(String s) throws IOException, URISyntaxException {
+  public JSONArray readFile(String fileName) throws IOException, URISyntaxException {
     JSONArray a = null;
     ClassLoader loader = getClass().getClassLoader();
-    String content = new String(Files.readAllBytes(Paths.get(loader.getResource(s).toURI())));
+    byte[] bytes = Files.readAllBytes(Paths.get(loader.getResource(fileName).toURI()));
+    String content = new String(bytes);
     a = new JSONArray(content);
     return a;
   }
 
-  public void writeFile(String s, JSONArray a) throws IOException, URISyntaxException {
+  public void writeFile(String fileName, JSONArray a) throws IOException, URISyntaxException {
     String file = a.toString();
     ClassLoader loader = getClass().getClassLoader();
-    FileOutputStream fos = new FileOutputStream(Paths.get(loader.getResource(s).toURI()).toString());
+    FileOutputStream fos = new FileOutputStream(Paths.get(loader.getResource(fileName).toURI()).toString());
     fos.write(file.getBytes());
     fos.close();
   }
